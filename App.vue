8dabf38af3c4347faa639aeaf46f8c3290569d76
S-Onix <min269@hanmir.com>
2020-07-28 14:09:09 +0900

    App.vue를 main.js에 랜더링 과정

--- App.vue
+++ App.vue
@@ -1,11 +1,18 @@
+// 하나의 컴포넌트로 동작하게됨
 <template>
-    
+    <h1>{{ msg }}</h1>    
 </template>
 <script>
 export default {
-    
+    data () {
+        return {
+            msg : 'Hello Vue!'
+        }
+    }
 }
 </script>
-<style>
-
+<style scoped>
+h1 {
+    color: red;
+}
 </style>
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("bundle test");
+import Vue from "vue";
+import App from "./App.vue";
+
+new Vue({
+  el: "#app",
+  //createElement는 콜백함수
+  //render(createElement) {
+  //아래의 콜백함수는 최상위 컴포넌트를 인수로 받는다
+  //return createElement(App);
+  //},
+  render: (h) => h(App),
+});
