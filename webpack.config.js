8c138b6d10f3a6f0cb63d693c51272613aa25ae8
S-Onix <min269@hanmir.com>
2020-07-28 10:30:10 +0900

    vetur, vue-loader 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   entry: {
     app: path.join(__dirname, "main.js"),
   },
-  output: "",
-  module: {},
-  plugins: [],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+  // webpack의 중간 처리과정에서 동작된다.
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+  // 모듈이 처리된 이후 추후에 어떤작업을 진행해야하는지 배열로 명시해준다.
+  // 결과적으로 output으로 전달해준다.
+  plugins: [new VueLoaderPlugin()],
 };
